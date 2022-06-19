145d7125f036db079847a8790e80575e51a8a45d
Amy Aphiar <amyaphiar@gmail.com>
2022-06-19 23:47:18 +0100

    Amy Aphiar's solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 // complete the function
 function prime (num) {
-  // code goes here
+  if (num ===2) return true;
+  if (num <= 1) return false;
+
+  for (let i= 2; i < num; i++) {
+    if (num % i === 0) {
+    return false;
+      }
+  }
+  return true;
 }
 
 function solution (arg) {
